1dedc47f0969a5a8f99086fb9145405393867015
Quân <quan@foolography.com>
2021-08-17 01:50:14 +0200

    Update Network + AsyncAwait.swift

--- Sources/ClassClapNetwork/Network + AsyncAwait.swift
+++ Sources/ClassClapNetwork/Network + AsyncAwait.swift
@@ -9,8 +9,63 @@ import Foundation
 
 // MARK: - New Swift 5.5 Async await
 @available(swift 5.5)
+@available(iOS 15.0, *)
 extension Network {
-    @available(iOS 15.0, *)
+    
+    /// Call a HTTP request. All the error handlers will stop the function immidiately
+    /// - Parameters:
+    ///   - method: HTTP method, `POST` in default
+    ///   - url: plain string of the url.
+    ///   - authorization: the authorization method, such as bearer token for example
+    ///   - params: http request body's parameters.
+    /// - Returns: Data and HTTP response.
+    public func sendRequest(
+        as method: Method = .post,
+        to link: String,
+        timeout: TimeInterval = 60.0,
+        authorization: Authorization? = nil,
+        parameters: [String : Any?]? = nil
+    ) async throws -> (Data, HTTPURLResponse) {
+        
+        // create request
+        guard let requestResult = try? createRequest(
+            from: link,
+            as: method,
+            timeout: timeout,
+            authorization: authorization
+        )
+        else {
+            throw NetworkError.badUrl
+        }
+        
+        var request = requestResult.request
+        let encodedUrl = requestResult.encodedUrl
+        
+        // config request
+        do {
+            try requestConfig(
+                &request,
+                method: method,
+                encodedUrl: encodedUrl,
+                parameters: parameters
+            )
+        } catch {
+            throw error
+        }
+        
+        // try to get data from request
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard
+            let httpResponse = response as? HTTPURLResponse,
+            HTTPStatus(httpResponse.statusCode) == .success
+        else {
+            throw NetworkError.transportError
+        }
+        
+        return (data, httpResponse)
+    }
+    
     /// Get the expected JSON - codable object via a HTTP request.
     /// - Parameters:
     ///   - method: the desired `HTTP method`.
